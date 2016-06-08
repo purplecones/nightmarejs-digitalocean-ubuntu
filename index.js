298e5a6c7f44d29233d9d1f46b6ea138de8c41f8
mjoldic <mjoldic@capfundr.com>
2016-06-08 11:59:11 -0400

    simplified

--- index.js
+++ index.js
@@ -1,29 +1,26 @@
-var CronJob = require('cron').CronJob;
 var Nightmare = require('nightmare');
 
-new CronJob('* * * * *', function() {
-  console.log('starting nightmarejs');
-  var nightmare = Nightmare({
-    show: false,
-    webPreferences: {
-      partition: 'nopersist'
-    }
-  });
+console.log('starting nightmarejs');
+var nightmare = Nightmare({
+  show: false,
+  webPreferences: {
+    partition: 'nopersist'
+  }
+});
 
-  nightmare
-    .goto('http://yahoo.com')
-    .type('form[action*="/search"] [name=p]', 'github nightmare')
-    .click('form[action*="/search"] [type=submit]')
-    .wait('#main')
-    .evaluate(function () {
-      console.log('evaluate');
-      return document.querySelector('#main .searchCenterMiddle li a').href;
-    })
-    .end()
-    .then(function (result) {
-      console.log(result);
-    })
-    .catch(function (error) {
-      console.error('Search failed:', error);
-    });
-}, null, true, 'America/Los_Angeles');
+nightmare
+  .goto('http://yahoo.com')
+  .type('form[action*="/search"] [name=p]', 'github nightmare')
+  .click('form[action*="/search"] [type=submit]')
+  .wait('#main')
+  .evaluate(function () {
+    console.log('evaluate');
+    return document.querySelector('#main .searchCenterMiddle li a').href;
+  })
+  .end()
+  .then(function (result) {
+    console.log(result);
+  })
+  .catch(function (error) {
+    console.error('Search failed:', error);
+  });
